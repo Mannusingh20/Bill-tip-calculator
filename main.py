909056b0f071f715390d9a4042b1b70968e40f6d
Mannusingh20 <125277097+Mannusingh20@users.noreply.github.com>
2023-03-28 16:15:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+kul_log = input ("Enter the number of people: ")
+kul_bill = input ("Enter the total bill:₹")
+tip_percent = input("Enter the the tip percent: ")
+
+tip = float(tip_percent)/100 * float(kul_bill)
+
+total = tip + float(kul_bill)
+per_person = total / int(kul_log)
+
+print(f"\nInitial bill = ₹{kul_bill}\nTip percent = {tip_percent}%\nTip = ₹{tip}\nTotal bill = ₹{total}\nPer person bill = ₹{round(per_person,2)}")
